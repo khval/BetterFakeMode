f13d5a2b60e6eafcb49f4b7cfb72fa430a8846ed
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-04-26 20:00:46 +0200

    Init.c is now built with -D__USE_INLINE__

--- init.c
+++ init.c
@@ -13,16 +13,16 @@ struct IntuitionIFace		*IIntuition = NULL;
 struct Library			*GraphicsBase = NULL;
 struct GraphicsIFace		*IGraphics = NULL;
 
-#define Printf IDOS->Printf
+APTR video_mutex = NULL;
 
 BOOL open_lib( const char *name, int ver , const char *iname, int iver, struct Library **base, struct Interface **interface)
 {
 	*interface = NULL;
-	*base = IExec -> OpenLibrary( name , ver);
+	*base = OpenLibrary( name , ver);
 
 	if (*base)
 	{
-		 *interface = IExec -> GetInterface( *base,  iname , iver, TAG_END );
+		 *interface = GetInterface( *base,  iname , iver, TAG_END );
 		if (!*interface) Printf("Unable to getInterface %s for %s %ld!\n",iname,name,ver);
 	}
 	else
@@ -38,16 +38,26 @@ bool open_libs()
 	if ( ! open_lib( "intuition.library", 51L , "main", 1, &IntuitionBase, (struct Interface **) &IIntuition  ) ) return FALSE;
 	if ( ! open_lib( "graphics.library", 54L , "main", 1, &GraphicsBase, (struct Interface **) &IGraphics  ) ) return FALSE;
 
+	video_mutex = (APTR) AllocSysObjectTags(ASOT_MUTEX, TAG_DONE);
+	if ( ! video_mutex) return FALSE;
+
+
 	return TRUE;
 }
 
 void close_libs()
 {
-	if (IntuitionBase) IExec -> CloseLibrary(IntuitionBase); IntuitionBase = 0;
-	if (IIntuition) IExec -> DropInterface((struct Interface*) IIntuition); IIntuition = 0;
+	if (video_mutex) 
+	{
+		FreeSysObject(ASOT_MUTEX, video_mutex); 
+		video_mutex = NULL;
+	}
 
-	if (GraphicsBase) IExec -> CloseLibrary(GraphicsBase); GraphicsBase = 0;
-	if (IGraphics) IExec -> DropInterface((struct Interface*) IGraphics); IGraphics = 0;
+	if (IntuitionBase) CloseLibrary(IntuitionBase); IntuitionBase = 0;
+	if (IIntuition) DropInterface((struct Interface*) IIntuition); IIntuition = 0;
+
+	if (GraphicsBase) CloseLibrary(GraphicsBase); GraphicsBase = 0;
+	if (IGraphics) DropInterface((struct Interface*) IGraphics); IGraphics = 0;
 }
 
 
