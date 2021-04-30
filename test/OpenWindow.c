51e5d1c76b84f47becff13147724c5261b061101
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-04-30 18:15:34 +0200

    Open Window test (even if it crashes)

---
+++ test/OpenWindow.c
@@ -0,0 +1,90 @@
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdarg.h>
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/intuition.h>
+#include <proto/graphics.h>
+#include <exec/emulation.h>
+
+#include "init.h"
+
+struct TextAttr MyFont =
+{
+	"topaz.font",
+	TOPAZ_SIXTY,
+	FS_NORMAL,
+	FPF_ROMFONT
+};
+
+struct NewScreen myscr = 
+	{
+		0,0,
+		320,
+		200,
+		2,
+		0,1,
+		(ULONG) NULL,
+		CUSTOMSCREEN,
+		&MyFont,
+		"My Own Screen",
+		NULL,
+		NULL
+	};
+
+void set_colors( struct Screen *src )
+{
+	struct BitMap *bm = src -> RastPort.BitMap;
+	int colors = 1L << bm -> Depth;
+	int c;
+
+	for (c=0;c<colors;c++) SetRGB32( &src -> ViewPort, 0xFFFFFFFF, 0xFFFFFFFF,0xFFFFFFFF,0xFFFFFFFF );
+}
+
+
+void pplot( struct BitMap *bm, int x, int y)
+{
+	int bx = 7- (x&7) ;
+	x /= 8;
+
+	*(bm -> Planes[0] + x + y * bm -> BytesPerRow ) |= 1L<<bx;
+}
+
+int main()
+{
+	struct Screen *src;
+	struct RastPort *rp;
+
+	if (open_libs()==FALSE)
+	{
+		Printf("failed to open libs!\n");
+		close_libs();
+		return 0;
+	}
+
+	src = OpenScreen( &myscr );
+
+	if (src)
+	{
+		struct Window *win = OpenWindowTags( NULL, 
+			WA_Width, 100,
+			WA_Height, 100,
+			WA_CustomScreen, src,
+			TAG_END);
+
+
+
+		getchar();
+
+		CloseWindow( win );
+
+		CloseScreen( src ) ;
+	}
+
+	close_libs();
+}
+
