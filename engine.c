1aeaca16b6db498ac51bccb3c32c68f51dfd1997
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-05-09 10:35:26 +0200

    Wrote a faster conversion routine for p2c.

--- engine.c
+++ engine.c
@@ -11,6 +11,7 @@
 #include <proto/dos.h>
 #include <proto/intuition.h>
 #include <proto/graphics.h>
+#include <proto/layers.h>
 #include <exec/emulation.h>
 #include <exec/ports.h>
 
@@ -30,6 +31,8 @@ struct amiga_rgb
 };
 
 uint32 argb[256];
+
+void comp_window_update( struct BitMap *bitmap, struct Window *win);
 
 void update_argb_lookup( struct ColorMap *cm )
 {
@@ -64,52 +67,157 @@ void draw_bits(struct RastPort *rp, unsigned char *b, int x,int  y )
 
 }
 
-void draw_screen( struct Window *win, struct BitMap *bm )
-{
-	struct RastPort *rp = win -> RPort;
-	int SizeOfPlane;
-	int x,y,p,d;
-	int bx,bpr;
-	int yy;
 
-	unsigned char *ptr;
-	uint64 *at;
+void draw_bits_argb( unsigned char *dest_ptr,unsigned int dest_bpr, unsigned char *b, int x,int  y )
+{
+	uint32 *d_argb = (uint32 *)	(dest_ptr + dest_bpr * y + (x*4));
+
+	*d_argb++ = argb[ *b++ ]; // 0
+	*d_argb++ = argb[ *b++ ]; // 1
+	*d_argb++ = argb[ *b++ ]; // 2
+	*d_argb++ = argb[ *b++ ]; // 3
+	*d_argb++ = argb[ *b++ ]; // 4
+	*d_argb++ = argb[ *b++ ]; // 5
+	*d_argb++ = argb[ *b++ ]; // 6
+	*d_argb++ = argb[ *b++ ]; // 7
+}
+
+uint64 draw_1p(unsigned char *ptr, int SizeOfPlane)
+{
+	return *((uint64 *) bits2bytes[*ptr]);
+}
+
+uint64 draw_2p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data = *((uint64 *) bits2bytes[*ptr]);
+	return data | *((uint64 *) bits2bytes[256+*(ptr+SizeOfPlane)]);
+}
+
+uint64 draw_3p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data =	*((uint64 *) bits2bytes[*ptr]);			ptr+= SizeOfPlane;
+	data |=		*((uint64 *) bits2bytes[1*256+*ptr]);	ptr+= SizeOfPlane;
+	return data |	*((uint64 *) bits2bytes[2*256+*ptr]);
+}
+
+uint64 draw_4p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data = *((uint64 *) bits2bytes[*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[1*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[2*256+*ptr]); ptr+= SizeOfPlane;
+	return data | *((uint64 *) bits2bytes[3*256+*(ptr)]);
+}
+
+uint64 draw_5p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data = *((uint64 *) bits2bytes[*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[1*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[2*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[3*256+*ptr]); ptr+= SizeOfPlane;
+	return data | *((uint64 *) bits2bytes[4*256+*(ptr)]);
+}
+
+uint64 draw_6p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data = *((uint64 *) bits2bytes[*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[1*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[2*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[3*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[4*256+*ptr]); ptr+= SizeOfPlane;
+	return data | *((uint64 *) bits2bytes[5*256+*(ptr)]);
+}
+
+uint64 draw_7p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data = *((uint64 *) bits2bytes[*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[1*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[2*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[3*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[4*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[5*256+*ptr]); ptr+= SizeOfPlane;
+	return data | *((uint64 *) bits2bytes[6*256+*(ptr)]);
+}
+
+uint64 draw_8p(unsigned char *ptr, int SizeOfPlane)
+{
+	uint64 data = *((uint64 *) bits2bytes[*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[1*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[2*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[3*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[4*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[5*256+*ptr]); ptr+= SizeOfPlane;
+	data |= *((uint64 *) bits2bytes[6*256+*ptr]); ptr+= SizeOfPlane;
+	return data | *((uint64 *) bits2bytes[7*256+*(ptr)]);
+};
+
+// basicly unrolled routines.
+
+void *planar_routines[]  =
+{
+	NULL,
+	draw_1p,
+	draw_2p,
+	draw_3p,
+	draw_4p,
+	draw_5p,
+	draw_6p,
+	draw_7p,
+	draw_8p
+};
+
+
+void draw_screen( struct Window *win, struct BitMap *bm, struct BitMap *dest_bm )
+{
+//	struct RastPort *rp = win -> RPort;
+	int SizeOfPlane;
+	int x,y;
+	int bx,bpr;
+	unsigned char *yptr;
 	uint64 data = 0;
 
-	d = bm -> Depth;
+	ULONG dest_format;
+	unsigned char *dest_ptr;
+	unsigned int dest_bpr;
+	APTR lock;
 
 	bpr = bm -> BytesPerRow;
 	SizeOfPlane = bm -> BytesPerRow * bm -> Rows;
 
+	uint64 (*planar_routine) (unsigned char *ptr, int SizeOfPlane) = NULL;
+
+	planar_routine = planar_routines[ bm -> Depth ];
+
+	lock = LockBitMapTags( dest_bm,
+			LBM_PixelFormat, &dest_format,
+			LBM_BytesPerRow, &dest_bpr,
+			LBM_BaseAddress, &dest_ptr,
+			TAG_END	 );
+
 	for (y=0;y<bm -> Rows; y++)
 	{
-		yy = y +win -> BorderTop;
-		x = win -> BorderLeft;
+		x = 0;
+		yptr = bm -> Planes[0] + bpr*y;
+
 		for (bx=0;bx<bpr;bx++)
 		{
-			data = 0;
-
-			ptr = bm -> Planes[0] + bpr*y + bx;
-
-			for (p=0;p<d;p++)
-			{
-				at = (uint64 *) bits2bytes[256*p+*ptr];
-				data |= *at;
-				ptr += SizeOfPlane;			
-			}
-
-			draw_bits( rp, (unsigned char *) &data ,  x,  yy );
-
+			data = planar_routine( yptr + bx, SizeOfPlane );
+			draw_bits_argb( dest_ptr,dest_bpr, (unsigned char *) &data ,  x,  y );
 			x+=8;
 		}
 	}
+
+	UnlockBitMap( lock );
 }
+
+
+
 
 void dump_screen()
 {
 	struct TimerContext tc;
 	struct Screen *src;
 	ULONG win_mask = 0;
+	struct BitMap *dest_bitmap;
 
 	bzero( &tc , sizeof(struct TimerContext) );
 
@@ -119,8 +227,11 @@ void dump_screen()
 			WA_DragBar, TRUE,
 			TAG_END);
 
-
 	if (!win) return ;
+
+	dest_bitmap =AllocBitMap( 640, 480, 32, BMF_DISPLAYABLE, win ->RPort -> BitMap);
+
+	if (!dest_bitmap) return ;
 
 	if (open_timer_context( &tc)== false)
 	{
@@ -144,9 +255,11 @@ void dump_screen()
 			if (src)
 			{
 				update_argb_lookup( src -> ViewPort.ColorMap );
-				draw_screen( win, src -> RastPort.BitMap );
+				draw_screen( win,  src -> RastPort.BitMap, dest_bitmap );
 			}
 			MutexRelease(video_mutex);
+
+			comp_window_update( dest_bitmap, win);
 
 			reset_timer( tc.timer_io );
 		}
@@ -155,6 +268,65 @@ void dump_screen()
 
 	close_timer_context( &tc );
 	CloseWindow( win );
+	FreeBitMap( dest_bitmap );
 }
 
+
+struct XYSTW_Vertex3D { 
+float x, y; 
+float s, t, w; 
+}; 
+
+void comp_window_update( struct BitMap *bitmap, struct Window *win)
+{
+	#define STEP(a,xx,yy,ss,tt,ww)   P[a].x= xx; P[a].y= yy; P[a].s= ss; P[a].t= tt; P[a].w= ww;  
+
+	int width,height;
+
+	int ww,wh;
+	int error;
+	float sx;
+	float sy;
+
+	float wx;
+	float wy;
+
+	struct XYSTW_Vertex3D P[6];
+
+	width=640;
+	height=480;
+
+	wx = win->BorderLeft + win -> LeftEdge;
+	wy = win->BorderTop + win -> TopEdge;
+
+	ww = win->Width - win->BorderLeft - win->BorderRight;
+	wh = win->Height -  win->BorderTop - win->BorderBottom;
+
+	STEP(0, wx, wy ,0 ,0 ,1);
+	STEP(1, wx+ww,wy,width,0,1);
+	STEP(2, wx+ww,wy+wh,width,height,1);
+
+	STEP(3, wx,wy, 0,0,1);
+	STEP(4, wx+ww,wy+wh,width,height,1);
+	STEP(5, wx, wy+wh ,0 ,height ,1);
+
+	LockLayer( 0 , win -> RPort -> Layer);
+
+	error = CompositeTags(COMPOSITE_Src, 
+			bitmap, win->RPort -> BitMap,
+
+			COMPTAG_VertexArray, P, 
+			COMPTAG_VertexFormat,COMPVF_STW0_Present,
+		    	COMPTAG_NumTriangles,2,
+
+			COMPTAG_ScaleX, (uint32) ( (float) 0x0010000 * sx ),
+			COMPTAG_ScaleY, (uint32) ( (float) 0x0010000 * sy ),
+
+			COMPTAG_SrcAlpha, (uint32) (0x0010000 ),
+			COMPTAG_Flags, COMPFLAG_SrcAlphaOverride | COMPFLAG_HardwareOnly | COMPFLAG_SrcFilter ,
+			TAG_DONE);
+
+	UnlockLayer( win -> RPort -> Layer );
+
+}
 
--- engine.h
+++ engine.h
@@ -1,7 +1,7 @@
 
 typedef unsigned char u8;
 
-void draw_screen( struct Window *win, struct BitMap *bm );
+void draw_screen( struct Window *win, struct BitMap *bm, struct BitMap *dest_bm );
 
 void dump_screen();
 
