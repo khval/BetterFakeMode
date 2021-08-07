f4c54116f60612fe69d69e2486c267910e9e9bc6
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2021-08-07 23:54:23 +0200

    Fixed the binary file generator.

--- tests/copper/gen_binary.c
+++ tests/copper/gen_binary.c
@@ -8,14 +8,22 @@ int main()
 	int  n;
 	char txt[32];
 	char *t;
-
 	int b,p;
+
+	for (n=0;n<0x100;n++)
+	{
+		t=txt;
+		for (b=1<<7;b;b>>=1,p++) *t++=(n&b)?'1':'0';
+		*t = 0;
+		printf("#define B%s %d\n",txt,n);
+	}
+
 	for (n=0;n<0x10000;n++)
 	{
 		t=txt;
 		for (b=1<<15;b;b>>=1,p++) *t++=(n&b)?'1':'0';
 		*t = 0;
-		printf("#define B%s\n",txt);
+		printf("#define B%s %d\n",txt,n);
 	}
 
 	printf("\n");
